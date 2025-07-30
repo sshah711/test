528f62f4b9f86fb3734df3a3b38c3cf705ff1b49
sshah711 <sshah380008@gmail.com>
2025-07-30 22:03:49 +0530

    m

---
+++ test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const WelcomeMessage = (props) => {
+  return (   
+
+    <h1>Welcome, {props.name}!</h1>
+
+  );
+};
+export default WelcomeMessage;
